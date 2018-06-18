bec10d5e32ee78f29959aca1d515f9dfaf1cd5cb
Solay <ssolay@gmail.com>
2018-06-17 23:45:22 -0400

    Add more data to empdetail.ts
    Added more Experience

--- src/app/_services/empdetail.ts
+++ src/app/_services/empdetail.ts
@@ -10,13 +10,13 @@ export const CHARACTERS: any[] =
   {
     Technology: 'Angular 2/4/6',
     Description:'',
-    Years: '1',
+    Years: '2',
   
   },
   {
     Technology: 'TypeScript',
     Description:'',
-    Years: '1',
+    Years: '2',
 
   },
   {
@@ -35,6 +35,18 @@ export const CHARACTERS: any[] =
     Technology: 'Apache Tomcat',
     Description:'',
     Years: '5',
+
+  },
+  {
+    Technology: 'JQuery',
+    Description:'',
+    Years: '1',
+
+  },
+  {
+    Technology: 'Spring MVC',
+    Description:'',
+    Years: '1',
 
   },
 ]
